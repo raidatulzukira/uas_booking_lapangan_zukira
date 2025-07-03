7d366e7a4131825f7654cf4a1646875a2d2cd428
raidatulzukira <raidatulzukiraa@gmail.com>
2025-07-04 02:10:00 +0700

    memperbaiki admin panel

--- app/Filament/Resources/ZukiraLapanganResource/Pages/CreateZukiraLapangan.php
+++ app/Filament/Resources/ZukiraLapanganResource/Pages/CreateZukiraLapangan.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateZukiraLapangan extends CreateRecord
 {
     protected static string $resource = ZukiraLapanganResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
